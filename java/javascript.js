fb6be8dba11e16fd1981be11b79d705bf751559f
Knight <janae.jpeg@gmail.com>
2020-01-10 19:00:01 -0600

    frickity frick update

--- java/javascript.js
+++ java/javascript.js
@@ -1,6 +1,6 @@
-var start = document.createElement("span");
-start.innerHTML = "You hear beeping.";
-document.getElementById("textspeak").appendChild(start);
+// DYNAMICALLY ADDING TEXT VIA JQUERY
+$('#textspeak').append("<span>You hear beeping.</span>");
+    // $('#textspeak').append("<span></span>");
 
 // Of course you couldn't. 
 // Of course you weren't <i>really</i> wandering.
@@ -12,25 +12,94 @@ document.getElementById("textspeak").appendChild(start);
 // For some reason, she couldn't remember your name.
 // ?? What's your name, anyway?
 
-var positive = ["yes", "y", "you bet", "yeehaw", "fuck yeah"];
-var negative = ["no","n", "fuck no"];
-var actions = ["watch", "listen", "get", "scream", "talk", "approach", "walk", "grab", "touch", "feel", "snooze", "sleep", "stop"];
-
-let input = document.getElementById("input");
+var input = document.getElementById("input");
 
 let points = 0; let name = "";
 
 input.addEventListener("keyup", function(event) {
+
+    // Gets/Defines input
     let thisInput = document.getElementById("input").value;
-    if (event.keyCode === 13) {
 
-        if (points===0) {
-            console.log("Progress: " +points, "/ 107");
-            // document.getElementById("input").innerHTML = "";
+    // Checks if p/n/pa/a keyword(s) are used
+    var positive = ["yes", "y", "you bet", "yeehaw", "fuck yeah", "alright"];
+    var negative = ["no","n", "fuck no", "nope"];
+    var passive = ["so", "ok", "okay","whatever", "cool", "nice", "k"];
+    var actions = ["watch", "listen", "get", "scream", "talk", "approach", "walk", "grab", "touch", "feel", "snooze", "sleep", "stop", "smash", "wake"];
+
+    function checkPositiveKey() {
+        if ($.inArray(thisInput, positive) !=-1) {
+            return true;
+        } else {
+            return false;
         }
+    }
+    function checkNegativeKey() {
+        if ($.inArray(thisInput, negative) !=-1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    function checkPassiveKey() {
+        if ($.inArray(thisInput, passive) !=-1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    function defaultPositiveMessage() {
+        if (checkPositiveKey()) {
+            var defPos = [
+                "You sound quite positive.",
+                "You seem rather joyous.",
+                "You seem very sure."
+            ];
+            var protoPositiveMessage = defPos[Math.floor(Math.random()*defPos.length)];
+            $('#textspeak').append("<span>"+protoPositiveMessage+"</span>");
+        }
+    }
+    function defaultNegativeMessage() {
+        if (checkNegativeKey()) {
+            var defNeg = [
+                "You seem rather negative.",
+                "You don't seem sure."
+            ];
+            var protoNegativeMessage = defNeg[Math.floor(Math.random()*defNeg.length)];
+            $('#textspeak').append("<span>"+protoNegativeMessage+"</span>");
+        }
+    }
+    function defaultPassiveMessage() {
+        if (checkPassiveKey()) {
+            var defPass = [
+                "You seem very passive.",
+                "You seem nonchalant."
+            ];
+            var protoPassiveMessage = defPass[Math.floor(Math.random()*defPass.length)];
+            $('#textspeak').append("<span>"+protoPassiveMessage+"</span>");
+        }
+    }
+    // On.enter container
+        // > doCommand()
+        // > printInput
+
+    if (event.keyCode === 13) {
+        $('#textspeak').append("<span>> " +thisInput+ "</span>");
+        checkPositiveKey(); checkNegativeKey(); checkPassiveKey();
+        doCommand();
     } else { event.preventDefault(); }
+
+    // Game/Progress Container
+
+    function doCommand() {
+        if (points === 0) {
+            defaultPositiveMessage(); defaultNegativeMessage();
+            defaultPassiveMessage()
+        }
+    }
 });
+
 
 
 
@@ -47,20 +116,3 @@ input.addEventListener("keyup", function(event) {
 
  // HAHAHAHAHAH ------>     PROMPT
                      //     The story is about an outgoing fire fighter, an introverted wanderer, and an electrician who is obsessed with a prostitute. It takes place in a city-sized magical device. The crux of the story involves a lecture.
-
-
-
-
-
-
-
-
-
-
-
-
-function blah() {
-    console.log("Hmmmmm");
-}
-
-setInterval(blah, 15000);
